9172150e70238796b9bd4c4ba7f0e00aad1914e1
Rashad <rashad007.cp@gmail.com>
2026-03-19 17:09:21 +0530

    t

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -52,6 +52,8 @@ export const About = () => {
                             alt="Muhammed Rashad"
                             className="w-full h-full object-cover filter grayscale sepia-[0.3] contrast-[1.1] group-hover:filter-none transition-all duration-700 transform group-hover:scale-105"
                         />
+
+                        
                         <span className="absolute bottom-4 left-4 text-[10px] tracking-widest uppercase font-mono text-white opacity-0 group-hover:opacity-100 transition-opacity z-20">
                             / Personal Profile.
                         </span>
